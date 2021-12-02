780653d7cd732df0f1b6b1d85542b819ec629106
hunterMotko <hunterMotko@gmail.com>
2021-12-02 15:28:57 -0500

    resetup private

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,6 @@
 class ArticlesController < ApplicationController
-
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -13,13 +12,12 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create 
     # render plain: params[:article]
     # ! cant use params[:article] to pass in info because it has to be white listed
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     @article.user = User.first
     # render plain: @article.inspect
     # @article.save
@@ -36,12 +34,21 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = 'Article was updated!'
       redirect_to @article
     else
       render 'edit'
     end
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description, category_ids: [])
   end
 end
