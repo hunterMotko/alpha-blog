36adc6843833785212013f6eb31fa91bffe5f114
hunterMotko <hunterMotko@gmail.com>
2021-12-16 12:27:39 -0500

    user authenication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user, only: [:show, :edit, :update]
+
   def show
-    @user = User.find(params[:id])
+    
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   
@@ -14,11 +15,11 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+    
   end
 
   def update 
-    @user = User.find(params[:id])
+    
     if @user.update(user_params)
       flash[:notice] = "Your account info was updated"
       redirect_to @user
@@ -30,6 +31,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome #{@user.username} to the Alpha Blog, successful signup!"
       redirect_to @user
     else
@@ -42,5 +44,7 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-  
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
