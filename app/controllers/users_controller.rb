d8f564afea8bd5917835330548c9748cc832a95d
hunterMotko <hunterMotko@gmail.com>
2022-02-14 14:17:30 -0500

    up to join forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: %i[show edit update destroy]
+  before_action :require_user, only: %i[edit update]
+  before_action :require_same_user, only: %i[edit update destroy]
 
   def show
-    
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
-  
+
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
@@ -13,13 +14,11 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def edit
-    
   end
 
-  def update 
-    
+  def update
     if @user.update(user_params)
       flash[:notice] = "Your account info was updated"
       redirect_to @user
@@ -39,12 +38,27 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil if @user == current_user
+    flash[:notice] = 'Account and all articles deleted'
+    redirect_to root_path
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password)
+    params.require(:user).permit(:username, :email, :password, :admin)
   end
+
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user && !current_user.admin?
+      flash[:alert] = 'You can only edit or delete your own account'
+      redirect_to @user
+    end
   end
 end
